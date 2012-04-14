2e482599aee954fc2f2a863753275b709a949507
Matthias Geisler <matthias@semanticdreamer.com>
2012-04-14 21:54:28 +0200

    fixed issue with daemon install

--- manifests/daemon.pp
+++ manifests/daemon.pp
@@ -2,18 +2,18 @@ class build-couchdb::daemon {
 
     case $build-couchdb::daemon {
         true: {
-            # exec { "install daemon":
-            #    command  => "ln -s ${build-couchdb::install_dir}/build-couchdb/build/etc/init.d/couchdb /etc/init.d/couchdb",
-            #    path     => [ "/bin/" ],
-            #    creates  => "/etc/init.d/couchdb",
-            #    subscribe => Exec["build couchdb"]
-            # }
+            exec { "install daemon":
+               command  => "ln -s ${build-couchdb::install_dir}/build-couchdb/build/etc/init.d/couchdb /etc/init.d/couchdb",
+               path     => [ "/bin/" ],
+               creates  => "/etc/init.d/couchdb",
+               subscribe => Exec["build couchdb"]
+            }
             
             file {
                 '/etc/init.d/couchdb':
                 ensure  => 'link',
                 target  => '${build-couchdb::install_dir}/build-couchdb/build/etc/init.d/couchdb',
-                require => Exec['build couchdb']
+                require => Exec['install daemon']
             }
             
             service {
