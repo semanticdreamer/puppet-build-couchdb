b0d32e5034e6ec2e148f1b07654f675bbd9d3796
Matthias Geisler <matthias@semanticdreamer.com>
2012-06-13 16:17:23 +0200

    avoiding duplicate package definition

--- manifests/build.pp
+++ manifests/build.pp
@@ -5,10 +5,12 @@ class build-couchdb::build {
    }
 
    notice("install packages git-core make gcc zlib1g-dev libssl-dev rake")
-   package {
-       ['git-core', 'make', 'gcc', 'zlib1g-dev', 'libssl-dev', 'rake']:
-       ensure => 'installed'
-   }
+   if ! defined(Package['git-core'])   { package { 'git-core':   ensure => installed } }
+   if ! defined(Package['make'])       { package { 'make':       ensure => installed } }
+   if ! defined(Package['gcc'])        { package { 'gcc':        ensure => installed } }
+   if ! defined(Package['zlib1g-dev']) { package { 'zlib1g-dev': ensure => installed } }
+   if ! defined(Package['libssl-dev']) { package { 'libssl-dev': ensure => installed } }
+   if ! defined(Package['rake'])       { package { 'rake':       ensure => installed } }
 
    notice ("\n\nNext step - build CouchDB from source.\n\n**** This process may take some time, so please be patient and relax...! ****\n\nIn case you're curious what's going on:\n\n1) fire up 2nd terminal and have a look at the working directory '${build-couchdb::install_dir}/build-couchdb',\n\n2) 'tail -f ${build-couchdb::install_dir}/build-couchdb/rake.log' to watch rake building couchdb...\n\n")
    exec { "build couchdb":
